49b92e7626cab53f6eb005fed1d153e38019b7e5
Iritscen <iritscen@yahoo.com>
2019-11-30 15:55:19 -0500

    Corrected potential path issues in get_source_length.sh.

--- Bash/get_source_length.sh
+++ Bash/get_source_length.sh
@@ -4,10 +4,12 @@
 # Prints the total lines of code in the current directory (recursive).
 # File name pattern to search for is hardcoded below.
 
+IFS="
+"
+
 COUNT=0
-for FN in `find . | grep "\.[cmh]$"`
-do
-   CUR=`cat $FN | wc -l`
+for FN in `find . | grep "\.[cmh]$"`; do
+   CUR=`cat "$FN" | wc -l`
    COUNT=`echo $CUR+$COUNT | bc`
 done
 echo $COUNT
