57e34534269722e3701ecf36bd6d2585ed1c0dff
Iritscen <iritscen@yahoo.com>
2019-12-06 12:01:02 -0500

    get_data_totals.sh now supports excluded subdirs and Catalina’s boot disk partition scheme.

--- Bash/get_data_totals.sh
+++ Bash/get_data_totals.sh
@@ -14,26 +14,29 @@
 #
 # /-------------------------------------\
 # |# Input file for get_data_totals.sh	|
-# |# Data set: All my data		|
+# |# Data set: Apps and photos		|
 # |					|
 # |vol=My Disk 1			|
 # |type=Photos				|
-# |~/Pictures				|
-# |~/Documents/My photos		|
+# |$HOME/Pictures			|
+# |$HOME/Documents/My photos		|
+# |-$HOME/Documents/My photos/RAWs	|
 # |					|
-# |type=iTunes music			|
-# |~/Music/iTunes			|
+# |type=Applications			|
+# |/Applications			|
 # |					|
 # |vol=My Disk 2			|
-# |type=iTunes music			|
-# |/iTunes Music			|
+# |type=Games				|
+# |/Games				|
 # \-------------------------------------/
 #
 # As seen above, comments are marked with the '#' character, "vol=" sets the current
 # volume to the name given, and "type=" is used to group paths under one name. All paths
 # should start with '/' and are relative to the volume name previously declared. An
-# exception to starting with a '/' is if you are using a shortcut like '~', which will
-# expand to have a '/' at the start. The use of blank lines as whitespace is optional.
+# exception to starting with a '/' is if you are using the shortcut '~' or "$HOME", which
+# will expand to have a '/' at the start. A leading '-' character tells the script to
+# deduct the size of the path from the type's total. The use of blank lines as whitespace
+# is optional.
 #
 # You can pass the script different input files for different sets of data that you wish
 # to tabulate. The script currently does not total types of data across volumes, only
@@ -88,10 +91,11 @@ else
 fi
 
 # Init global variables
-declare -a VOL_NAMES=() # parallel array with next three
+declare -a VOL_NAMES=() # parallel array with next four
 declare -a DATA_TYPES=()
 declare -a DIR_PATHS=()
 declare -a DATA_SIZES=()
+declare -a DEDUCT_IT=()
 NUM_PATHS=0
 
 # Print a raw number of bytes at a human-readable scale
@@ -165,6 +169,15 @@ for THE_LINE in `echo "$INPUT_DATA"`; do
       continue
    fi
 
+   # If this line is asking to be deducted from the total, set the "deduct" flag and
+   # remove the '-' from the beginning
+   if [[ $THE_LINE =~ ^- ]]; then
+      THE_LINE=${THE_LINE#-}
+      DEDUCT_IT+=(1)
+   else
+      DEDUCT_IT+=(0)
+   fi
+
    # Otherwise this must be a directory path, so read it in, invoking 'eval' in order to
    # expand shortcuts like '~' or "$HOME", then make sure it exists
    THE_PATH="$(eval echo "$THE_LINE")"
@@ -186,14 +199,29 @@ done
 a=0
 echo -n "Getting sizes of directories ($a/$NUM_PATHS paths processed)..."
 while [ "x${DIR_PATHS[$a]}" != "x" ]; do
+   # If 'du' is being asked to get the size of /System, mask out the subdir "Volumes"
+   # since this is a link in Catalina to the entire volume
    FULL_PATH="/Volumes/${VOL_NAMES[$a]}${DIR_PATHS[$a]}"
-   let SIZE_SECTORS=`sudo du -sc "$FULL_PATH" 2> /dev/null | tail -1 | cut -f 1`
+   DU_CALL="sudo du -sc '$FULL_PATH' 2> /dev/null | tail -1 | cut -f 1"
+   if [ "${DIR_PATHS[$a]}" == "/System" ]; then
+      DU_CALL="sudo du -sc -IVolumes '$FULL_PATH' 2> /dev/null | tail -1 | cut -f 1"
+   fi
+
+   # Ask 'du' for the size of the path
+   let SIZE_SECTORS=$(eval $DU_CALL)
    let SIZE_BYTES=$SIZE_SECTORS*512
    DATA_SIZES[$a]=$SIZE_BYTES
+
+   # Invert number if this is a "deduct" line
+   if [ ${DEDUCT_IT[$a]} -eq 1 ]; then
+      let DATA_SIZES[$a]*=-1
+   fi
+
+   # Update status
    let a+=1
    printf "\e[1A\n" # erase previous "processed..." message
    PATHS_STR="paths"
-   if [ $a -eq 1 ]; then
+   if [ $NUM_PATHS -eq 1 ]; then
       PATHS_STR="path"
    fi
    echo -n "Getting sizes of directories ($a/$NUM_PATHS $PATHS_STR processed)..."
@@ -222,6 +250,26 @@ while [ "x${DIR_PATHS[$a]}" != "x" ]; do
       # Get used space on this volume
       USED_BLOCKS=${DF_LINE[2]}
       let USED_BYTES=$USED_BLOCKS*512
+
+      # Check if this is the boot volume; if so, then in Catalina we need to add two
+      # other mount points' used figures to get the total used space on the disk
+      DISK_NAME=${DF_LINE[0]}
+      declare -a DF_ROOT_LINE=(`df / | tail -1`)
+      ROOT_VOL_NAME=${DF_ROOT_LINE[0]}
+      if [ "$DISK_NAME" == "$ROOT_VOL_NAME" ]; then
+         if [ -d /System/Volumes/Data ]; then
+            declare -a DF_DATA_LINE=(`df /System/Volumes/Data | tail -1`)
+            USED_BLOCKS=${DF_DATA_LINE[2]}
+            let USED_BYTES+=$USED_BLOCKS*512
+         fi
+         if [ -d /private/var/vm ]; then
+            declare -a DF_VM_LINE=(`df /private/var/vm | tail -1`)
+            USED_BLOCKS=${DF_VM_LINE[2]}
+            let USED_BYTES+=$USED_BLOCKS*512
+         fi
+      fi
+
+      # Convert our total used bytes to something readable
       USED=$(printHumanReadable $USED_BYTES)
 
       # Print total line for last volume
@@ -236,7 +284,7 @@ while [ "x${DIR_PATHS[$a]}" != "x" ]; do
       else
          echo -ne "\n"
       fi
-      echo -ne "Volume: ${VOL_NAMES[$a]} ($USED/$CAPAC GB used)"
+      echo -ne "Volume: ${VOL_NAMES[$a]} ($USED/$CAPAC used)"
 
       # Reset stuff
       LAST_VOL=${VOL_NAMES[$a]}
