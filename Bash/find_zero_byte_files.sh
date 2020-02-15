ecd1bbeb51922419db45a0e71479a49caa4534fb
Iritscen <iritscen@yahoo.com>
2020-02-14 21:48:12 -0500

    Tweaked comment in find_zero_byte_files.sh.

--- Bash/find_zero_byte_files.sh
+++ Bash/find_zero_byte_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Find Zero-Byte Files
-# Lists all files in the current directory that are zero bytes in size.
+# Recursively lists all files, starting from the current directory, that are zero bytes in size.
 
 ls -ilR | egrep ^[[:digit:]]+[[:space:]]+[drwx@-]+[[:space:]]+[[:digit:]]+[[:space:]]+[[:alpha:]]+[[:space:]]+[[:alpha:]]+[[:space:]]+0
