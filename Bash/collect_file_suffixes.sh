3c9cc3d5b8f0e9e4c8c75f8bac980f8922700220
Iritscen <iritscen@yahoo.com>
2020-03-05 09:22:21 -0500

    collect_file_suffixes.sh now also prints the count of files with each suffix.

--- Bash/collect_file_suffixes.sh
+++ Bash/collect_file_suffixes.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
 # Collect File Suffixes
-# A script which collects and prints out all the unique file
-# suffixes in a directory. The only parameter required is the
-# directory to recursively search.
+# A script which collects and prints out all the unique file suffixes in a directory,
+# and the count of files with each suffix. The only parameter required is the
+# directory in which to recursively look.
 
 IFS="
 "
 
 SEARCH_PATH=$1
 declare -a SUFFIXES=()
+declare -a SUFFIX_COUNTS=()
 LOOKED_AT=0
 
 if [ ! -d "$SEARCH_PATH" ]; then
    echo "Error: \"$SEARCH_PATH\" does not exist. Aborting."
    exit
 fi
+
+# The final output of suffixes and their counts is put in a function so we can
+# collect all the output and sort it
+function printSuffixes()
+{
+   for ((i = 0; i < ${#SUFFIXES[@]}; ++i)); do
+      STR_FILES="files"
+      if [ ${SUFFIX_COUNTS[$i]} -eq 1 ]; then
+         STR_FILES="file"
+      fi
+      printf "%s (%s $STR_FILES)\n" ${SUFFIXES[$i]} ${SUFFIX_COUNTS[$i]}
+   done
+}
 
 for FILE in `find "$SEARCH_PATH" -type f`; do
    FILE_NAME=$(echo "$FILE" | sed 's/.*\///') # clip file name from whole path
@@ -30,8 +44,9 @@ for FILE in `find "$SEARCH_PATH" -type f`; do
    # Search for suffix in collection we've made so far
    shopt -s nocasematch
    COLLECTED=0
-   for SUFFIX in "${SUFFIXES[@]}"; do
-      if [ "$SUFFIX" == $FILE_SUFFIX ]; then
+   for ((i = 0; i < ${#SUFFIXES[@]}; ++i)); do
+      if [ "${SUFFIXES[$i]}" == $FILE_SUFFIX ]; then
+         let SUFFIX_COUNTS[$i]+=1
          COLLECTED=1
       fi
    done
@@ -40,10 +55,11 @@ for FILE in `find "$SEARCH_PATH" -type f`; do
    # If we haven't seen this suffix before now, add it to the array
    if [ $COLLECTED -eq 0 ]; then
       SUFFIXES+=($FILE_SUFFIX)
+      SUFFIX_COUNTS+=(1)
    fi
 
    let LOOKED_AT+=1
 done
 
-declare -a SORTED_SUFFIXES=($(sort <<< "${SUFFIXES[*]}"))
-echo "Looked at $LOOKED_AT files. The suffixes found were: ${SORTED_SUFFIXES[@]}."
+echo "Looked at $LOOKED_AT files. The suffixes found were:"
+printSuffixes | sort
